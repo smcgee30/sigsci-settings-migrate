649e0b5fbeb77d5e6152f9e804712d82957b15bd
smcgee30 <smcgee30@gmail.com>
2017-12-01 10:37:02 -0500

    Vars

--- Exportconfig/run.py
+++ Exportconfig/run.py
@@ -4,12 +4,15 @@ import json
 
 postreqdata = json.loads(open(os.environ['req']).read())
 response = open(os.environ['res'], 'w')
-req = {
-   'appname' : postreqdata['appname'],
-   'boardname' :  postreqdata['boadname'],
-   'maincontact' :  postreqdata['maincontact']
-}
+# req = {
+#   'appname' : postreqdata['appname'],
+#   'boardname' :  postreqdata['boadname'],
+#   'maincontact' :  postreqdata['maincontact']
+#}
 
-json_str = json.dumps(data)
-response.write(json_str)
+#json_str = json.dumps(data)
+#response.write(json_str)
+
+response.write(os.environ['req_query_appname'] + os.environ['req_query_boardname'])
+
 response.close()
